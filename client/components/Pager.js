570a10c536bd42a1ff55aa3b2634157be8cf7b9e
Theo Manton <theo333@gmail.com>
2019-04-24 18:30:39 -0400

    converted Pager to class

--- client/components/Pager.js
+++ client/components/Pager.js
@@ -2,20 +2,14 @@ import React, { Component } from 'react';
 
 // TODO - change to class because have a class method - gotoPage
 
-export default ({ currentPage, totalPages, history, match }) => {
-	const current = currentPage || 0;
-	// const _current
-	console.log('pager match: ', match);
-	console.log('history: ', history);
-	console.log('totalPages: ', totalPages);
-	console.log('currentPage: ', currentPage);
-	const { pathname } = history.location;
-	// TODO - when do hard reload on search page, does not show user data
-	const gotoPage = pageNum => {
-		// console.log('pageNum: ', pageNum);
-		// console.log('current: ', current);
+export default class Pager extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	gotoPage = pageNum => {
+		const { match, history } = this.props;
 		let path = '';
-		// if (location.pathname.includes('/search')) {
 		if (match.params.term) {
 			path = `/users/search/${match.params.term}/${pageNum}`;
 		} else {
@@ -23,50 +17,46 @@ export default ({ currentPage, totalPages, history, match }) => {
 		}
 		return history.push(path);
 	};
-	return (
-		<div id='pager' className='btn-group'>
-			<button
-				onClick={() => gotoPage(0)}
-				className='btn btn-info'
-				// disabled={
-				// 	pathname === '/users' || pathname === '/users/0' ? ' disabled' : ''
-				// }
-				disabled={current === 1 ? ' disabled' : ''}
-			>
-				First
-			</button>
-			<button
-				onClick={() => gotoPage(current - 2)}
-				className='btn btn-info'
-				// disabled={
-				// 	pathname === '/users' || pathname === '/users/0' ? ' disabled' : ''
-				// }
-				disabled={current === 1 ? ' disabled' : ''}
-			>
-				Prev
-			</button>
-			<span className='btn btn-primary'>{currentPage}</span>
-			<button
-				onClick={() => gotoPage(current)}
-				className='btn btn-info'
-				disabled={current === totalPages ? ' disabled' : ''}
-			>
-				Next
-			</button>
-			{/* <button
-				onClick={() => gotoPage(totalPages - 1)}
-				className='btn btn-info'
-				disabled={pathname === `/users/${totalPages - 1}` ? ' disabled' : ''}
-			>
-				Last
-			</button> */}
-			<button
-				onClick={() => gotoPage(totalPages - 1)}
-				className='btn btn-info'
-				disabled={current === totalPages ? ' disabled' : ''}
-			>
-				Last
-			</button>
-		</div>
-	);
-};
+
+	render() {
+		const { gotoPage } = this;
+		const { currentPage, totalPages } = this.props;
+		const current = currentPage || 0;
+
+		// TODO - when do hard reload on search page, does not show user data
+
+		return (
+			<div id='pager' className='btn-group'>
+				<button
+					onClick={() => gotoPage(0)}
+					className='btn btn-info'
+					disabled={current === 1 ? ' disabled' : ''}
+				>
+					First
+				</button>
+				<button
+					onClick={() => gotoPage(current - 2)}
+					className='btn btn-info'
+					disabled={current === 1 ? ' disabled' : ''}
+				>
+					Prev
+				</button>
+				<span className='btn btn-primary'>{currentPage}</span>
+				<button
+					onClick={() => gotoPage(current)}
+					className='btn btn-info'
+					disabled={current === totalPages ? ' disabled' : ''}
+				>
+					Next
+				</button>
+				<button
+					onClick={() => gotoPage(totalPages - 1)}
+					className='btn btn-info'
+					disabled={current === totalPages ? ' disabled' : ''}
+				>
+					Last
+				</button>
+			</div>
+		);
+	}
+}
