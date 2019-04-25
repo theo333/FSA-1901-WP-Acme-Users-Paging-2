c5bdcc3d44673f277952db712a7ab7212119478b
Theo Manton <theo333@gmail.com>
2019-04-25 13:13:25 -0400

    renamed Highlight

--- client/components/UsersTable.js
+++ client/components/UsersTable.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { displayHighlight } from './Highlight';
+import { displayHighlight } from './HighlightHelper';
 
 const UsersTable = ({ users, term }) => {
 	const tableHeaders = [
